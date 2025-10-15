572d614ce8dbc7b33d6c30a630665e125a67091b
Jacob Sullins <sullinsje@etsu.edu>
2025-10-15 13:53:27 -0400

    Initial commit

---
+++ StudentTrackerAPI/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace StudentTrackerAPI.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
---
+++ StudentTrackerMvc/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace StudentTrackerMvc.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
