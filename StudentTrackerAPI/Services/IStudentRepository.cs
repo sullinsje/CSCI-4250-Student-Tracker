9404496b65b8d3786691f1ac11e409d38131984c
Jacob Sullins <sullinsje@etsu.edu>
2025-10-16 10:06:17 -0400

    Removed all the Views/wwwroot stuff for the API. I also added a shell for the classes and services and stuff. We need to plan out our database tables fully before we start I think

---
+++ StudentTrackerAPI/Services/IStudentRepository.cs
@@ -0,0 +1,13 @@
+using StudentTrackerAPI.Models.Entities;
+
+namespace StudentTrackerAPI.Services;
+
+public interface IStudentRepository
+{
+    // Task<ICollection<Student>> ReadAllAsync();
+    // Task<Student> CreateAsync(Student newStudent);
+    // Task<Student?> ReadAsync(int id);
+    // Task UpdateAsync(int oldId, Student updatedStudent);
+    // Task DeleteAsync(int id);
+    
+}
