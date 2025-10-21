2682356835c341c2cd0310a41db9ce2f6ee675a5
Jacob Sullins <sullinsje@etsu.edu>
2025-10-20 22:03:15 -0400

    Student API finished, added swagger for testing. I populated our database file but we might share it outside of github for 'good practice'

--- StudentTrackerAPI/Models/Entities/Student.cs
+++ StudentTrackerAPI/Models/Entities/Student.cs
@@ -2,6 +2,6 @@ namespace StudentTrackerAPI.Models.Entities;
 
 public class Student
 {
-    public int id {get; set;}
+    public int id { get; set; }
     public string name { get; set; } = String.Empty;
 }
