ff3cf3f63dd145c0906d5f2b0a01e96bc353f73a
Toyakazu <williamsme4@etsu.edu>
2025-11-03 10:48:37 -0500

    Fixed Controller to run Login

--- StudentTrackerMvc/Controllers/StudentController.cs
+++ StudentTrackerMvc/Controllers/StudentController.cs
@@ -24,6 +24,11 @@ public class StudentController : Controller
         return View();
     }
 
+    public IActionResult Login()
+    {
+        return View();
+    }
+
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
     {
