a275e34a10b19227bd40c744b15658b0c01755ed
Toyakazu <williamsme4@etsu.edu>
2025-11-03 10:42:28 -0500

    Removed HTML templates and added in the new CSHTML homepages and login pages.

--- StudentTrackerMvc/Controllers/TeacherController.cs
+++ StudentTrackerMvc/Controllers/TeacherController.cs
@@ -26,6 +26,10 @@ public class TeacherController : Controller
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     
+    public IActionResult Login()
+    {
+        return View();
+    }
     public IActionResult Error()
     {
         return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
